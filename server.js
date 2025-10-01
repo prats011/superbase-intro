cec6225d8abd43b10f32c187ab8b351e45b6af7b
prats011 <jhaveripratham2006@gmail.com>
2025-10-01 08:52:46 +0700

    +post, get methods

--- server.js
+++ server.js
@@ -12,3 +12,29 @@ const app = express();
 const PORT = 3000;
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+}); 
+
+app.post("/signup", async (req, res) => {
+    const { email, password } = req.body;
+    const {user, error} = await supabase.auth.signUp({ email, password });
+
+    if(error) return res.redirect(`/error.html?msg=${encodeURIComponent(error.message)}`);
+    res.redirect("/signup_success.html")
+});
+
+app.post("/login", async(req, res) => {
+    const { email, password } = req.body;
+    const {data, error } = await supabase.auth.signInWithPassword({ email, password });
+
+    if(error) return res.redirect(`/error.html?msg=${encodeURIComponent(error.message)}`);
+    
+})
+
